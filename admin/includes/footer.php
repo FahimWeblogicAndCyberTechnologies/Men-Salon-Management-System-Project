36f4a297b9a5657a4a9de69e834a85954eb3838c
FahimWeblogicAndCyberTechnologies <weblogicfahim@gmail.com>
2021-09-10 07:49:56 -0400

    Men Salon Management System Project

---
+++ admin/includes/footer.php
@@ -0,0 +1,5 @@
+<!--footer-->
+    <div class="footer">
+       <p>&copy; 2021 Men's Salon Management System Admin Panel.</p>
+    </div>
+        <!--//footer-->
---
+++ appointment.php
@@ -0,0 +1,210 @@
+<?php 
+include('includes/dbconnection.php');
+session_start();
+error_reporting(0);
+include('includes/dbconnection.php');
+if(isset($_POST['submit']))
+  {
+
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $services=$_POST['services'];
+    $adate=$_POST['adate'];
+    $atime=$_POST['atime'];
+    $phone=$_POST['phone'];
+    $aptnumber = mt_rand(100000000, 999999999);
+  
+    $query=mysqli_query($con,"insert into tblappointment(AptNumber,Name,Email,PhoneNumber,AptDate,AptTime,Services) value('$aptnumber','$name','$email','$phone','$adate','$atime','$services')");
+    if ($query) {
+$ret=mysqli_query($con,"select AptNumber from tblappointment where Email='$email' and  PhoneNumber='$phone'");
+$result=mysqli_fetch_array($ret);
+$_SESSION['aptno']=$result['AptNumber'];
+ echo "<script>window.location.href='thank-you.php'</script>";  
+  }
+  else
+    {
+     echo "<script>alert('Something Went Wrong. Please try again.');</script>"; 
+    }
+
+  
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   
+    <title>Men Salon Management System || Appointments Form</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,300i,400,400i,500,500i,700,700i%7cMontserrat:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <!-- Style -->
+    <link href="css/style.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+    <?php include_once('includes/header.php');?>
+    <div class="page-header">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <div class="page-caption">
+                        <h2 class="page-title">Book Appointment</h2>
+                        <div class="page-breadcrumb">
+                            <ol class="breadcrumb">
+                                <li><a href="index.php">Home</a></li>
+                                <li class="active">Book Appointment</li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="content">
+        <div class="container">
+            <div class="row">
+                
+                <div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <h1>Appointment Form</h1>
+                            <p> Book your appointment to save salon rush.</p>
+                            <form method="post">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <label class="control-label" for="name">Name</label>
+                                        <input type="text" class="form-control" id="name" placeholder="Name" name="name" required="true">
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label class="control-label" for="phone">phone</label>
+                                        <input type="text" class="form-control" id="phone" name="phone" placeholder="Phone" required="true" maxlength="10" pattern="[0-9]+">
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label class="control-label" for="email">email</label>
+                                         <input type="email" class="form-control" id="appointment_email" placeholder="Email" name="email" required="true">
+                                    </div>
+                                    <div class="col-md-6">
+                                        <label class="control-label" for="Subject">Services</label>
+                                        <select name="services" id="services" required="true" class="form-control">
+                                <option value="">Select Services</option>
+                                <?php $query=mysqli_query($con,"select * from tblservices");
+              while($row=mysqli_fetch_array($query))
+              {
+              ?>
+                               <option value="<?php echo $row['ServiceName'];?>"><?php echo $row['ServiceName'];?></option>
+                               <?php } ?> 
+                              </select>
+                                    </div>
+                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                        <div class="form-group">
+                                            <label class="control-label" for="textarea">Appointment Date</label>
+                                            <input type="date" class="form-control appointment_date" placeholder="Date" name="adate" id='adate' required="true">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                                        <div class="form-group">
+                                            <label class="control-label" for="textarea">Appointment Time</label>
+                                            <input type="time" class="form-control appointment_time" placeholder="Time" name="atime" id='atime' required="true">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="form-group">
+                                            <button type="submit" id="submit" name="submit" class="btn btn-default">Book</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+        <!-- footer-->
+        <div class="container">
+            <div class="footer-block">
+                <!-- footer block -->
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                        <div class="footer-widget">
+                            <h2 class="widget-title">Salon Address</h2>
+                            <ul class="listnone contact">
+                                <li><i class="fa fa-map-marker"></i> 4958 Norman Street Los Angeles, CA 90042 </li>
+                                <li><i class="fa fa-phone"></i> +00 (800) 123-4567</li>
+                                <li><i class="fa fa-fax"></i> +00 (123) 456 7890</li>
+                                <li><i class="fa fa-envelope-o"></i> info@salon.com</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
+                        <div class="footer-widget footer-social">
+                            <!-- social block -->
+                            <h2 class="widget-title">Social Feed</h2>
+                            <ul class="listnone">
+                                <li>
+                                    <a href="#"> <i class="fa fa-facebook"></i> Facebook </a>
+                                </li>
+                                <li><a href="#"><i class="fa fa-twitter"></i> Twitter</a></li>
+                                <li><a href="#"><i class="fa fa-google-plus"></i> Google Plus</a></li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i> Linked In</a></li>
+                                <li>
+                                    <a href="#"> <i class="fa fa-youtube"></i>Youtube</a>
+                                </li>
+                            </ul>
+                        </div>
+                        <!-- /.social block -->
+                    </div>
+                    <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
+                        <div class="footer-widget widget-newsletter">
+                            <!-- newsletter block -->
+                            <h2 class="widget-title">Newsletters</h2>
+                            <p>Enter your email address to receive new patient information and other useful information right to your inbox.</p>
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Email Address">
+                                <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">Subscribe</button>
+                            </span>
+                            </div>
+                            <!-- /input-group -->
+                        </div>
+                        <!-- newsletter block -->
+                    </div>
+                </div>
+                <div class="tiny-footer">
+                    <!-- tiny footer block -->
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                            <div class="copyright-content">
+                                <p>© Men Salon 2020 | all rights reserved</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /.tiny footer block -->
+            </div>
+            <!-- /.footer block -->
+        </div>
+    </div>
+    <!-- /.footer-->
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="js/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/menumaker.js"></script>
+    <script src="js/jquery.sticky.js"></script>
+    <script src="js/sticky-header.js"></script>
+</body>
+
+</html>
